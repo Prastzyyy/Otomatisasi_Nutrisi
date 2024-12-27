fbc97d3cb63c406b8d7a960766a94cba8e91689a
Prasetyo Ade <prasetyoadep123@gmail.com>
2024-12-27 14:08:56 +0700

    Add files via upload

---
+++ NUTRISI_OTOMATIS_ALL/Spreadsheet_info.ino
@@ -0,0 +1,24 @@
+//Program ESP_Spreadsheet Otomatisasi Nutrisi
+
+/*
+
+Google Sheets Project Name      : Data_Greenhouse
+Link Google SpreadSheet         : https://docs.google.com/spreadsheets/d/1IKQU8h4dVo9KKv69Chr1sNzfcQOmlh4LeF0Fe2pe_4k/edit?gid=0#gid=0
+Google Sheets ID                : 1IKQU8h4dVo9KKv69Chr1sNzfcQOmlh4LeF0Fe2pe_4k
+Sheet Name                      : Data_Otomatisasi_Nutrisi
+
+Data Logger
+Date | Time | pH | PPM | Ketinggian Air | Suhu | Status
+
+Latest Data Logger
+Date | Time | pH | PPM | Ketinggian Air | Suhu | Status
+
+
+Google Apps Script Project Name : Otomatisasi Nutrisi
+Web app URL            : https://script.google.com/macros/s/AKfycbymkMA_twgLD93tI3Wus_LB1lhZvrY-fx7ugi3x2M_GG6CAOzrnzLYfxIs_uPW0b3ysZg/exec
+
+Web app URL Test Write : https://script.google.com/macros/s/AKfycbxFSnaUGqLeOhUedsSziM5v7tSQLlaGGI7ivoKx1QAX9lEWjpGvjEmqA2FVVZ21GXtvcw/exec?sts=write&pH=123&PPM=321&tinggi_cm=678&suhu=345&pompaPenyiram=abc
+
+Web app URL Test Read  : https://script.google.com/macros/s/AKfycbxFSnaUGqLeOhUedsSziM5v7tSQLlaGGI7ivoKx1QAX9lEWjpGvjEmqA2FVVZ21GXtvcw/exec?sts=read
+
+*/
